71a6dfcb1985b5e814ce9e5e22974204f53922a3
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2022-01-12 21:40:17 -0300

    Bug no header resolvido
    bug do menu que após clicar no nome de usuário em conta de usuário comum ele não recebia a class active, isso só funcionava quando estava na conta de Admin.
    Link Admin adicionado ao menu e removido do nav no header.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,38 +1,14 @@
-// Nav
-let anotacoes = document.querySelector('.anotacoes')
-let metas = document.querySelector('.metas')
-let outros = document.querySelector('.outros')
-let clientes = document.querySelector('.clientes')
+// Script para abrir menu
+let perfil = document.querySelector('.perfil')
+let config = document.querySelector('.config')
+let fecharConfig = document.querySelector('.bx-chevron-right')
 
-// Menu
-let perfil = document.querySelector('.perfil');
-let config = document.querySelector('.config');
-let fechar = document.querySelector('.bx-chevron-right');
-
-// Script para o nav
-anotacoes.onclick = function () {
-    anotacoes.classList.toggle("active")
+perfil.onclick = function() {
+    perfil.classList.toggle('activePerfil')
+    config.classList.toggle('activeConfig')
 }
 
-metas.onclick = function () {
-    metas.classList.toggle("active")
-}
-
-outros.onclick = function () {
-    outros.classList.toggle("active")
-}
-
-clientes.onclick = function () {
-    clientes.classList.toggle("active")
-}
-
-// Script para o menu
-perfil.onclick = function () {
-    perfil.classList.toggle("activePerfil")
-    config.classList.toggle("activeConfig")
-}
-
-fechar.onclick = function(){
-    perfil.classList.remove("activePerfil")
-    config.classList.remove("activeConfig")
+fecharConfig.onclick = function () {
+    perfil.classList.toggle('activePerfil')
+    config.classList.toggle('activeConfig')
 }
