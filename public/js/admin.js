157c7263f268a1e7a3e0d29bbbc74807a9b08689
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2022-02-11 20:29:03 -0300

    abertura da div info na página de admin
    bug resolvido e seta adicionado direcionando a div

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,22 +1,9 @@
 function infoUser(el) {
-    document.getElementById(el).style.height = '78px'
-    document.getElementById(el).style.visibility = 'visible'    
-    document.querySelector(".bx-chevron-down").style.transform = "rotate(180deg)"
+    let info = document.getElementById(el);
+    info.classList.toggle('activeInfo');
 }
 
-function closeInfo(el) {
-    document.getElementById(el).style.height = '0px'
-    document.getElementById(el).style.visibility = 'hidden'
+function setaInfo(el) {
+    let seta = document.getElementById(el);
+    seta.classList.toggle('active')
 }
-
-// function infoUser(el) {
-//     let info = document.getElementById(el).style.visibility
-            
-//     if(info == "hidden") {
-//         document.getElementById(el).style.height = '78px'
-//         document.getElementById(el).style.visibility = 'visible'     
-//     } else {
-//         document.getElementById(el).style.height = '0px'
-//         document.getElementById(el).style.visibility = 'hidden'
-//     }
-// }
