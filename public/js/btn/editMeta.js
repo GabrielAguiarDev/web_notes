61090b980fdb4991f146393e261831423432868f
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2022-02-22 21:03:40 -0300

    página temas iniciada, e bug no confirmMetas resolvido

--- public/js/btn/editMeta.js
+++ public/js/btn/editMeta.js
@@ -19,16 +19,14 @@ function cancelarEdit(el) {
 }
 
 function metaSuccess(el){
-    document.getElementById(el).style.opacity = '1';
-    document.getElementById(el).style.zIndex = "1"
+    document.getElementById(el).classList.add("activeCard")
     document.getElementById('fundo').style.backdropFilter = "blur(1px)"
     document.getElementById('fundo').style.zIndex = "1"
     document.getElementById('fundo').style.pointerEvents = "auto"
 }
 
 function cancelarMetaSuccess(el) {
-    document.getElementById(el).style.opacity = '0';
-    document.getElementById(el).style.zIndex = "-1"
+    document.getElementById(el).classList.remove("activeCard")
     document.getElementById('fundo').style.backdropFilter = "blur(0px)"
     document.getElementById('fundo').style.zIndex = "0"
     document.getElementById('fundo').style.pointerEvents = "none"
