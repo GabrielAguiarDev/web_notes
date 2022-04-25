52db36e8f1e0b416d253d5c3132f76dc9144334d
Gabriel Aguiar <gabriel.aguiar.dev@gmail.com>
2022-04-24 23:58:38 -0300

    Modelo de mensagem de erro

---
+++ public/js/btn/msg.js
@@ -0,0 +1,8 @@
+var btnMsg = document.querySelector("#btnX")
+var divMsgErro = document.querySelector("#divMsgErro")
+
+
+function fecharErro(el) {
+    console.log(divMsgErro)
+    document.getElementById(el).style.right = "-460px";
+}
